ce297d52aa283955133b7287be4261569e18213a
rishh01 <rishabhsuryavanshi2004@gmail.com>
2024-06-27 12:39:20 +0530

    Initial Commit

---
+++ lib/Utils/colour.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppConst{
+
+  static const black=Colors.black;
+  static const white=Colors.white;
+  static const brown=Colors.brown;
+  static Color theme=Colors.amber.shade50;
+}
