3cffc8870c464eabc5ca58eb3c16fe9f367cc268
yashvi-dobariya <115923020+yashvi-dobariya@users.noreply.github.com>
2024-07-04 19:05:16 +0530

    Delete lib/Utils/font_style.dart

--- lib/Utils/font_style.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-TextStyle fontStyle(double size,Color color, FontWeight fw){
-  return GoogleFonts.poppins(fontSize: size,color: color,fontWeight: fw);
-}
