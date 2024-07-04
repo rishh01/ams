b8a668693abc81bb06f9c4186c6405d09e3f423f
yashvi-dobariya <115923020+yashvi-dobariya@users.noreply.github.com>
2024-07-04 19:04:33 +0530

    Delete lib/Utils/colour.dart

--- lib/Utils/colour.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppConst{
-
-  static const black=Colors.black;
-  static const white=Colors.white;
-  static const brown=Colors.brown;
-  static Color theme=Colors.amber.shade50;
-}
